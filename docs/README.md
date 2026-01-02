91ab088a8d7ed8b1a8a47c5e1a5fb147b28eeee8
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2026-01-02 11:54:48 -0500

    update docs

---
+++ docs/README.md
@@ -0,0 +1,9 @@
+# Demo
+
+This directory contains an interactive demo of the `@zachcp/molstar-components` package.  This shows use in HTMl using import maps. Components can also be used as standats TSX components within javascript projects.
+
+
+```bash
+# Using Python
+python -m http.server 8000
+```
---
+++ docs/demo-data.js
@@ -0,0 +1,67 @@
+// Example MVS data for the simple viewer
+export const exampleMVSData = {
+    root: {
+        kind: "root",
+        children: [
+            {
+                kind: "download",
+                params: {
+                    url: "https://www.ebi.ac.uk/pdbe/entry-files/1cbs.bcif",
+                },
+                children: [
+                    {
+                        kind: "parse",
+                        params: { format: "bcif" },
+                        children: [
+                            {
+                                kind: "structure",
+                                params: { type: "model" },
+                                children: [
+                                    {
+                                        kind: "component",
+                                        params: {
+                                            selector: "polymer",
+                                        },
+                                        children: [
+                                            {
+                                                kind: "representation",
+                                                params: {
+                                                    type: "cartoon",
+                                                },
+                                                children: [
+                                                    {
+                                                        kind: "color",
+                                                        params: {
+                                                            color: "green",
+                                                        },
+                                                    },
+                                                ],
+                                            },
+                                        ],
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    metadata: { title: "Example Structure", version: "1" },
+};
+
+// Default code for the editor
+export const defaultCode = `const structure = builder
+  .download({ url: 'https://www.ebi.ac.uk/pdbe/entry-files/1cbs.bcif' })
+  .parse({ format: 'bcif' })
+  .modelStructure();
+
+structure
+  .component({ selector: 'polymer' })
+  .representation({ type: 'cartoon' })
+  .color({ color: 'green' });
+
+structure
+  .component({ selector: 'ligand' })
+  .representation({ type: 'ball_and_stick' })
+  .color({ color: '#cc3399' });`;
